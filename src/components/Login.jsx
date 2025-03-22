34805bd5e62784089348f9c13d61c9ddb36ce490
Suhas Bachhav <ksb.suhas@gmail.com>
2025-03-22 13:47:14 +0530

    custom hooks and validation

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,49 +1,28 @@
-import { useState } from "react";
+import { useRef, useState } from 'react';
 
 export default function Login() {
+  const [emailIsInvalid, setEmailIsInvalid] = useState(false);
 
-  const [enteredValue, setEnteredValue] = useState({
-    email: '',
-    password: '',
-  });
-
-  const [didEdit, setDidEdit] = useState({
-    email: false,
-    password: false,
-  });
-
-  const emailIsInvalid = didEdit.email && !enteredValue.email.includes('@');
-  console.log(emailIsInvalid);
-
-  function handleInputChange(identifier, value) {
-    setEnteredValue((prevEnteredValue) => {
-      return {
-        ...prevEnteredValue,
-        [identifier]: value,
-      };
-    }); 
-    setDidEdit(prevDidEdit => ({
-      ...prevDidEdit,
-      [identifier]: false,
-    }));
-  }
+  const email = useRef();
+  const password = useRef();
 
   function handleSubmit(event) {
     event.preventDefault();
-    const form = event.target;
-    const email = form.email.value;
-    const password = form.password.value;
-    console.log(email, password);
+
+    const enteredEmail = email.current.value;
+    const enteredPassword = password.current.value;
+
+    const emailIsValid = enteredEmail.includes('@');
+
+    if (!emailIsValid) {
+      setEmailIsInvalid(true);
+      return;
+    }
+
+    setEmailIsInvalid(false);
+
+    console.log('Sending HTTP request...');
   }
-
-
-  function handleInputBlur(identifier) {
-    setDidEdit(prevDidEdit => ({
-      ...prevDidEdit,
-      [identifier]: true,
-    }));
-  }
-
 
   return (
     <form onSubmit={handleSubmit}>
@@ -52,17 +31,15 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" 
-          onBlur={() => handleInputBlur('email')}
-          onChange={(e) => handleInputChange('email', e.target.value)}/>
-          <div className="control-error">{emailIsInvalid && <p>Please enter a valid email address</p>}</div>
+          <input id="email" type="email" name="email" ref={email} />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email address.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" 
-          onBlur={() => handleInputBlur('password')}
-          onChange={(e) => handleInputChange('password', e.target.value)}/>
+          <input id="password" type="password" name="password" ref={password} />
         </div>
       </div>
 
--- src/components/Singup.jsx
+++ src/components/Singup.jsx
@@ -1,113 +1,133 @@
+import { useState } from 'react';
+
 export default function Signup() {
-    function handleSubmit(event) {
-        event.preventDefault();
+  const [passwordsAreNotEqual, setPasswordsAreNotEqual] = useState(false);
 
-        const formData = new FormData(event.target);
-        const acquisition = formData.getAll('acquisition');
-        const data = Object.fromEntries(formData.entries());
-        data.acquisition = acquisition; 
-        const form = event.target;
-        const terms = form.terms.checked;
-        data.terms = terms;
-        console.log(data);
+  function handleSubmit(event) {
+    event.preventDefault();
 
-        event.target.reset();
+    const fd = new FormData(event.target);
+    const acquisitionChannel = fd.getAll('acquisition');
+    const data = Object.fromEntries(fd.entries());
+    data.acquisition = acquisitionChannel;
+
+    if (data.password !== data['confirm-password']) {
+      setPasswordsAreNotEqual(true);
+      return;
     }
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <h2>Welcome on board!</h2>
-            <p>We just need a little bit of data from you to get you started 🚀</p>
+    console.log(data);
+  }
 
-            <div className="control">
-                <label htmlFor="email">Email</label>
-                <input id="email" type="email" name="email" />
-            </div>
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Welcome on board!</h2>
+      <p>We just need a little bit of data from you to get you started 🚀</p>
 
-            <div className="control-row">
-                <div className="control">
-                    <label htmlFor="password">Password</label>
-                    <input id="password" type="password" name="password" />
-                </div>
+      <div className="control">
+        <label htmlFor="email">Email</label>
+        <input id="email" type="email" name="email" required />
+      </div>
 
-                <div className="control">
-                    <label htmlFor="confirm-password">Confirm Password</label>
-                    <input
-                        id="confirm-password"
-                        type="password"
-                        name="confirm-password"
-                    />
-                </div>
-            </div>
+      <div className="control-row">
+        <div className="control">
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            required
+            minLength={6}
+          />
+        </div>
 
-            <hr />
+        <div className="control">
+          <label htmlFor="confirm-password">Confirm Password</label>
+          <input
+            id="confirm-password"
+            type="password"
+            name="confirm-password"
+            required
+          />
+          <div className="control-error">
+            {passwordsAreNotEqual && <p>Passwords must match.</p>}
+          </div>
+        </div>
+      </div>
 
-            <div className="control-row">
-                <div className="control">
-                    <label htmlFor="first-name">First Name</label>
-                    <input type="text" id="first-name" name="first-name" />
-                </div>
+      <hr />
 
-                <div className="control">
-                    <label htmlFor="last-name">Last Name</label>
-                    <input type="text" id="last-name" name="last-name" />
-                </div>
-            </div>
+      <div className="control-row">
+        <div className="control">
+          <label htmlFor="first-name">First Name</label>
+          <input type="text" id="first-name" name="first-name" required />
+        </div>
 
-            <div className="control">
-                <label htmlFor="phone">What best describes your role?</label>
-                <select id="role" name="role">
-                    <option value="student">Student</option>
-                    <option value="teacher">Teacher</option>
-                    <option value="employee">Employee</option>
-                    <option value="founder">Founder</option>
-                    <option value="other">Other</option>
-                </select>
-            </div>
+        <div className="control">
+          <label htmlFor="last-name">Last Name</label>
+          <input type="text" id="last-name" name="last-name" required />
+        </div>
+      </div>
 
-            <fieldset>
-                <legend>How did you find us?</legend>
-                <div className="control">
-                    <input
-                        type="checkbox"
-                        id="google"
-                        name="acquisition"
-                        value="google"
-                    />
-                    <label htmlFor="google">Google</label>
-                </div>
+      <div className="control">
+        <label htmlFor="phone">What best describes your role?</label>
+        <select id="role" name="role" required>
+          <option value="student">Student</option>
+          <option value="teacher">Teacher</option>
+          <option value="employee">Employee</option>
+          <option value="founder">Founder</option>
+          <option value="other">Other</option>
+        </select>
+      </div>
 
-                <div className="control">
-                    <input
-                        type="checkbox"
-                        id="friend"
-                        name="acquisition"
-                        value="friend"
-                    />
-                    <label htmlFor="friend">Referred by friend</label>
-                </div>
+      <fieldset>
+        <legend>How did you find us?</legend>
+        <div className="control">
+          <input
+            type="checkbox"
+            id="google"
+            name="acquisition"
+            value="google"
+          />
+          <label htmlFor="google">Google</label>
+        </div>
 
-                <div className="control">
-                    <input type="checkbox" id="other" name="acquisition" value="other" />
-                    <label htmlFor="other">Other</label>
-                </div>
-            </fieldset>
+        <div className="control">
+          <input
+            type="checkbox"
+            id="friend"
+            name="acquisition"
+            value="friend"
+          />
+          <label htmlFor="friend">Referred by friend</label>
+        </div>
 
-            <div className="control">
-                <label htmlFor="terms-and-conditions">
-                    <input type="checkbox" id="terms-and-conditions" name="terms" />I
-                    agree to the terms and conditions
-                </label>
-            </div>
+        <div className="control">
+          <input type="checkbox" id="other" name="acquisition" value="other" />
+          <label htmlFor="other">Other</label>
+        </div>
+      </fieldset>
 
-            <p className="form-actions">
-                <button type="reset" className="button button-flat">
-                    Reset
-                </button>
-                <button type="submit" className="button">
-                    Sign up
-                </button>
-            </p>
-        </form>
-    );
+      <div className="control">
+        <label htmlFor="terms-and-conditions">
+          <input
+            type="checkbox"
+            id="terms-and-conditions"
+            name="terms"
+            required
+          />
+          I agree to the terms and conditions
+        </label>
+      </div>
+
+      <p className="form-actions">
+        <button type="reset" className="button button-flat">
+          Reset
+        </button>
+        <button type="submit" className="button">
+          Sign up
+        </button>
+      </p>
+    </form>
+  );
 }
